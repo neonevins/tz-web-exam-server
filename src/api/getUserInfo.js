2f931502dbc9cc8b5823d3b7cc8b7a5126cae3eb
neonevins <244270720@qq.com>
2019-12-12 15:04:08 +0800

    添加查询参数

--- src/api/getUserInfo.js
+++ src/api/getUserInfo.js
@@ -2,11 +2,32 @@ const {UserTable} = require("../db/api/tables")
 // 只能根据自己的cookie获取自己的用户信息
 async function getUserInfo(req, res) {
   if(!req.cookies.token){
-    res.send({
-      code: 1,
-      message: '用户不存在, 请登录',
-      data: {}
-    })
+    if(req.query.token){
+      let token = req.query.token
+      let result = await UserTable.findOne({token})
+      if(!result){
+        res.send({
+          code: 1,
+          message: '用户不存在, 请清空缓存',
+          data: {}
+        })
+        return
+      }else{
+        let {avatarPath, sex, user, userType} = result
+        res.send({
+          code: 0,
+          message: '查询成功',
+          data: {avatarPath, sex, user, userType}
+        })
+      }
+    }else{
+      res.send({
+        code: 1,
+        message: '用户不存在, 请登录',
+        data: {}
+      })
+    }
+
     return
   }
   let token = req.cookies.token
