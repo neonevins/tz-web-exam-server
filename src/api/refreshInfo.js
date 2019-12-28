3b72215160478731106f83d45a728f54c500e2dd
neonevins <244270720@qq.com>
2019-12-28 18:36:19 +0800

    直接存base64

--- src/api/refreshInfo.js
+++ src/api/refreshInfo.js
@@ -6,42 +6,61 @@ async function refreshInfo(req, res) {
   const {nickname, sex, avatarPath} = req.body
   console.log({nickname, sex, avatarPath})
   // res.send({nickname, sex, avatarPath})
-  if(avatarPath.length > 100){
-    // 后缀
-    let extra = /^data:image\/(?<extra>\w+);base64,/.exec(avatarPath).groups.extra
-    let base64 = avatarPath.replace(/^data:image\/\w+;base64,/, "")
-    const buffer = Buffer.from(base64, 'base64')
-    let targetPath = path.resolve(__dirname, '../../uploads/avatar')
-    let filename = new Date().valueOf()
-    fs.writeFileSync(`${targetPath}/${filename}.${extra}`, buffer)
-    const realPath = `http://47.104.155.140/avatar/${filename}.${extra}`
-    await UserTable.updateOne(
-      {token:req.cookies.token},
-      {$set:{nickname, sex, avatarPath: realPath}})
-    res.send({
-      code: 0,
-      msg: "更新成功",
-      data: {
-        userInfo: {
-          nickname, sex, avatarPath: realPath
-        }
+
+
+  // 直接存base64
+  await UserTable.updateOne(
+    {token:req.cookies.token},
+    {$set:{nickname, sex, avatarPath}})
+  res.send({
+    code: 0,
+    msg: "更新成功",
+    data: {
+      userInfo: {
+        nickname, sex, avatarPath
       }
-    })
-  }else{
-    await UserTable.updateOne(
-      {token:req.cookies.token},
-      {$set:{nickname, sex}},
-      )
-    res.send({
-      code: 0,
-      msg: "更新成功",
-      data: {
-        userInfo: {
-          nickname, sex
-        }
-      }
-    })
-  }
+    }
+  })
+
+
+
+  //base64转图片的版本
+  // if(avatarPath.length > 100){
+  //   // 后缀
+  //   let extra = /^data:image\/(?<extra>\w+?);base64,/.exec(avatarPath).groups.extra
+  //   let base64 = avatarPath.replace(/^data:image\/\w+?;base64,/, "")
+  //   const buffer = Buffer.from(base64, 'base64')
+  //   let targetPath = path.resolve(__dirname, '../../uploads/avatar')
+  //   let filename = new Date().valueOf()
+  //   fs.writeFileSync(`${targetPath}/${filename}.${extra}`, buffer)
+  //   const realPath = `http://47.104.155.140/avatar/${filename}.${extra}`
+  //   await UserTable.updateOne(
+  //     {token:req.cookies.token},
+  //     {$set:{nickname, sex, avatarPath: realPath}})
+  //   res.send({
+  //     code: 0,
+  //     msg: "更新成功",
+  //     data: {
+  //       userInfo: {
+  //         nickname, sex, avatarPath: realPath
+  //       }
+  //     }
+  //   })
+  // }else{
+  //   await UserTable.updateOne(
+  //     {token:req.cookies.token},
+  //     {$set:{nickname, sex}},
+  //     )
+  //   res.send({
+  //     code: 0,
+  //     msg: "更新成功",
+  //     data: {
+  //       userInfo: {
+  //         nickname, sex
+  //       }
+  //     }
+  //   })
+  // }
 }
 
 module.exports = refreshInfo
