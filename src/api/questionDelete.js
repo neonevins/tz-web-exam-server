87c9f92406fe9768e0bae84d0bab6f462d44eeee
afeifeifei <53498870@qq.com>
2020-01-22 16:32:27 +0800

    question接口新增

---
+++ src/api/questionDelete.js
@@ -0,0 +1,52 @@
+/*
+* 题目删除
+*
+* GET
+* 前端参数：
+* {
+*   _id : String，必须，题目对应的_id
+* }
+*
+* */
+const question = require("../db/api/questions");
+module.exports = async (req,res)=>{
+    let _id = req.query;
+
+    let info = {};
+    if (
+        _id
+        && typeof _id === "string"
+    ){
+        /*删除*/
+        try{
+            let data = await question.deleteOne({_id});
+            if (data.n){
+                info = {
+                    code : 0,
+                    message: "操作成功。"
+                };
+            }else{
+                info = {
+                    code : 0,
+                    message : "操作成功，但未查询到对应数据",
+                    data
+                }
+            }
+        }catch (e) {
+            info = {
+                code : 4,
+                message : "服务器操作失败o(╥﹏╥)o"
+            };
+        }
+
+
+    }else{
+        /*_id字段出错*/
+        info = {
+            code : 1,
+            message : "_id字段类型错误！"
+        }
+    }
+
+    res.send(info);
+};
---
+++ src/api/questionUpdate.js
@@ -0,0 +1,60 @@
+/*
+* 题目更新接口
+* GET
+* 前端参数：
+* *
+*
+* {
+*   _id : String，必须
+*       ***题目id
+*   data : {
+*       type : 必须，string
+*           ***题型，可选："HTML" "CSS" "JS" "VUE" "REACT" "NODE"
+*       title : 必须，string
+*           ***题目
+*       code ： 可选，string
+*           ***题目所需的代码
+*       options : 必须，[string,string,…]
+*           ***选项数组，每一项为字符串
+*       rightOptions : 必须，[number,…]
+*           ***正确答案数组，每一项为数字（正确选项对应的序号），单选也需要传入数组但只需要一个序号数据
+*       analysis ： 可选，string
+*           ***题目解析，默认"略"
+*     }
+* }
+* */
+const question = require("../db/api/questions");
+module.exports = async (req,res)=>{
+    let {_id,data} = req.query;
+
+    let result = {};
+    if (
+        _id
+        && typeof _id === "string"
+        && data
+        && typeof data === "object"
+    ){
+        try{
+            let data = await question.updateOne({_id},data);
+            if (data.n){
+                result = {code:0,message:"更新成功。"};
+            }else{
+                result = {code:0,message:"操作成功，但是未查找到数据",data};
+            }
+        }catch (e) {
+            result = {code:1,message:"更新失败o(╥﹏╥)o",data:e}
+        }
+
+    }else{
+        result = {code:1,message:"参数格式错误！"}
+    }
+
+    res.send(result);
+};
+
+
+
+
+
+
+
